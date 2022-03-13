5b4877b2f1f0803f9b12cf878bb6cb199cb8cdf0
Josh Kenchel <josh.kenchel@gmail.com>
2022-03-13 16:36:58 -0700

    Add files via upload

---
+++ endoderm_GRN_model_tier.R
@@ -0,0 +1,155 @@
+# C. elegans endoderm development GRN model - Tier model
+# From "Recursive feedforward loops govern robust cell fate lockdown during C. elegans embryogenesis"
+# by Chee Kiang Ewe, Erica M. Sommermann, Josh Kenchel, Sagen E. Flowers, Morris F. Maduro, and Joel H. Rothman
+# UC Santa Barbara, UC Riverside, and UC Los Angeles
+# Updated 13 March 2022 by Josh Kenchel
+# jkenchel@ucsb.edu
+
+# Program should be ready to run as-is.
+# Flip logical vectors to make knockout mutants or adjust parameters as needed.
+# Program will create a plot of gene expression levels and print elt-2 expression metrics to the console.
+
+#Switches to make knockout mutants. Make TRUE to knock out. wt is all FALSE.
+skn1ko <- FALSE
+pop1ko <- FALSE
+med2ko <- FALSE
+med1ko <- FALSE
+end3ko <- FALSE
+end1ko <- FALSE
+elt7ko <- FALSE
+elt2ko <- FALSE
+
+# Model parameters described in supplementary table
+delta <- 3.278295e-02 #Native degradation rate
+a1 <- 4.666656e-03 #SKN-1 activation of med-2
+a2 <- 3.789606e-02 #SKN-1 activation of med-1
+a3 <- 2.546014e-02 #SKN-1 activation of end-3
+a4 <- 7.088873e-18 #SKN-1 activation of end-1
+a6 <- 3.473743e-02 #MED-2 activation of end-3
+a17 <- 4.436445e-18 #MED-2 activation of end-1
+a7 <- 2.181668e-17 #MED-1 activation of end-3
+a8 <- 2.048436e-01 #MED-1 activation of end-1
+a9 <- 7.410022e-02 #POP-1 activation of end-3
+a10 <- 8.742976e-17 #POP-1 activation of end-1
+a12 <- 1.074840e-01 #END-3 activation of elt-7
+a13 <- 1.559075e-17 #END-1 activation of elt-7
+a14 <- 5.219584e-02 #END-3 activation of elt-2 adjusted from fitted value of 2.219584e-18
+a15 <- 3.344716e-02 #END-1 activation of elt-2
+f1 <- 1.185834e-01 #ELT-7 feedback
+f2 <- 3.839391e-01 #ELT-2 feedback
+theta1 <- 1.216969e+02 #Threshold required for ELT-7 feedback (ppm)
+theta2 <- 2.141869e+01 #Threshold required for ELT-2 feedback (ppm)
+tau <- 5.184828e+01 #Transcription-activation delay (min)
+
+#Initial conditions (ppm)
+skn1_0 <- 23
+pop1_0 <- 3
+med2_0 <- 13.8
+med1_0 <- 5.6
+end3_0 <- 5.3
+end1_0 <- 0
+elt7_0 <- 0
+elt2_0 <- 0
+
+#SKN-1 and POP-1 conditions for square waves
+skn1on <- TRUE #at t=0
+skn1offTime <- 23 #min
+pop1on <- FALSE #at t=0
+pop1onTime <- 23 #min
+pop1offTime <- 41 #min
+
+# Time settings
+tEnd <- 1000 #total time of run, in min
+dt <- 0.01 #Time step for Euler approximation
+nTimepoints <- tEnd/dt + 1 #number of timepoints
+times <- seq(0, tEnd, by = dt) #vector of all timepoints
+txnDelayTimepoints <- round(tau/dt)
+
+#Creating vectors to store expression level time series for all genes
+med2 <- numeric(nTimepoints)
+med1 <- numeric(nTimepoints)
+end3 <- numeric(nTimepoints)
+end1 <- numeric(nTimepoints)
+elt7 <- numeric(nTimepoints)
+elt2 <- numeric(nTimepoints)
+med2[1] <- med2_0*!med2ko
+med1[1] <- med1_0*!med1ko
+end3[1] <- end3_0*!end3ko
+end1[1] <- end1_0*!end1ko
+elt7[1] <- elt7_0*!elt7ko
+elt2[1] <- elt2_0*!elt2ko
+
+# Loop through the number of timepoints given by the run time and time step dt
+# The main part of the model
+for(i in 1:(nTimepoints-1)){
+  
+  # Logical gates to create square waves for SKN-1 and POP-1
+  if(times[i] > skn1offTime){
+    skn1on <- FALSE
+  }
+  if(times[i] > pop1onTime){
+    pop1on <- TRUE
+  }
+  if(times[i] > pop1offTime){
+    pop1on <- FALSE
+  }
+  
+  # conditions at t_i
+  skn1 <- skn1_0*skn1on*!skn1ko
+  pop1 <- pop1_0*pop1on*!pop1ko
+  med2_i <- med2[i]
+  med1_i <- med1[i]
+  end3_i <- end3[i]
+  end1_i <- end1[i]
+  elt7_i <- elt7[i]
+  elt2_i <- elt2[i]
+  
+  # Calculation of timepoint used for activation due to transcription-activation delay
+  txnEffectTimepoint <- max(c(1, i - txnDelayTimepoints))
+  med2_prev <- med2[txnEffectTimepoint]
+  med1_prev <- med1[txnEffectTimepoint]
+  end3_prev <- end3[txnEffectTimepoint]
+  end1_prev <- end1[txnEffectTimepoint]
+  elt7_prev <- elt7[txnEffectTimepoint]
+  elt2_prev <- elt2[txnEffectTimepoint]
+  
+  # System of differential equations described in supplemental text
+  # Conditions at t_i used to calculate rates of change
+  dmed2 <- (skn1*a1 - delta*med2_i)*!med2ko
+  dmed1 <- (skn1*a2 - delta*med1_i)*!med1ko
+  dend3 <- (skn1*a3 + med1_prev*a7 + med2_prev*a6 + pop1*a9 - delta*end3_i)*!end3ko
+  dend1 <- (skn1*a4 + med2_prev*a17 + med1_prev*a8 + pop1*a10 - delta*end1_i)*!end1ko
+  delt7 <- (end1_prev*a13 + end3_prev*a12 + f1*(elt7_prev>theta1)*elt7_prev - delta*elt7_i)*!elt7ko
+  delt2 <- (end1_prev*a15 + end3_prev*a14 + f2*(elt2_prev>theta2)*elt2_prev - delta*elt2_i)*!elt2ko
+  
+  # Calculation of conditions at t_i+1 given the derivatives and time step dt
+  med2[i+1] <- med2_i + dt*dmed2
+  med1[i+1] <- med1_i + dt*dmed1
+  end3[i+1] <- end3_i + dt*dend3
+  end1[i+1] <- end1_i + dt*dend1
+  elt7[i+1] <- elt7_i + dt*delt7
+  elt2[i+1] <- elt2_i + dt*delt2
+}
+
+#Make graph of gene expression levels
+plot(y=med2, x=times, type = "l",
+     ylab = "[Transcript] (ppm)", xlab = "Time post 4-cell stage (min)", 
+     col = "red", ylim = c(0,200), xlim = c(0,200),
+     main = "Gene expression levels")
+lines(y=med1, x=times, type = "l", col = "orange")
+lines(y=end3, x=times, type = "l", col = "green")
+lines(y=end1, x=times, type = "l", col = "skyblue")
+lines(y=elt7, x=times, type = "l", col = "blue")
+lines(y=elt2, x=times, type = "l", col = "purple")
+legend("topleft", legend = c("med-2", "med-1", "end-3", "end-1", "elt-7", "elt-2"),
+       col = c("red", "orange", "green", "skyblue", "blue", "purple"), lwd = 1)
+
+#Print metrics of elt-2 timing
+#Arbitrary concentration threshold used for comparative metrics, based on transcriptomics data
+elt2timeToArbitraryConc <- min(which(floor(elt2) == 134))*dt
+#Arbitrary timepoint chosen to quantify elt-2 timing, again based on transcriptomics data
+elt2atArbitraryTimepoint <- elt2[min(which(floor(times) == 186))]
+elt2at260min <- elt2[min(which(floor(times) == 260))]
+cat("elt-2 time to 134 ppm transcript: ", elt2timeToArbitraryConc, "\n",
+    "[elt-2] at 186 min: ", elt2atArbitraryTimepoint, "\n",
+    "[elt-2] at 260 min: ", elt2at260min, "\n")
---
+++ fit_endoderm_GRN_model_tier.R
@@ -0,0 +1,289 @@
+# "Modified Gauss-Newton algorithm" to fit endoderm GRN Tier model to transcriptomics data
+# From "Recursive feedforward loops govern robust cell fate lockdown during C. elegans embryogenesis"
+# by Chee Kiang Ewe, Erica M. Sommermann, Josh Kenchel, Sagen E. Flowers, Morris F. Maduro, and Joel H. Rothman
+# UC Santa Barbara, UC Riverside, and UC Los Angeles
+# Updated 13 March 2022 by Josh Kenchel
+# jkenchel#ucsb.edu
+
+# *** Recommended to run in R STUDIO to keep all plots in one window. ***
+# Program takes parameter guesses, changes some parameter values randomly, runs model,
+# calculates sum of squared differences, and keeps new values if less than previous sum of squares.
+# Program iterates nGen times, changing parameter values with probability mu.
+# New parameter values are drawn from normal distribution with mean of current value and SD of mutationSD multiplied by current value.
+
+# IF RUNNING PROGRAM FOR FIRST TIME:
+#   It should be okay to run as-is.
+# IF ITERATING ON PREVIOUSLY EVOLVED VALUES STORED IN bestParamGuesses IN THE WORKSPACE:
+# set this value to TRUE
+iterating <- FALSE
+
+#Sets least squares value to Infinite if this is the first time running the program in this workspace
+if(!iterating){
+  leastSquares <- Inf
+}
+
+# Settings for evolution (fitting) of model parameters
+nGen <- 1000 #number of generations (cycles)
+mu <- 0.05 #probability that a parameter gets mutated to a new value
+
+# Given mutation, the standard deviation (as a percent of current value) of the normal distribution
+# with mean of the current value from which to draw a new value
+mutationSD <- 0.1
+
+# Parameter names described in the supplementary text and files
+paramNames <- c("delta", #Native degradation rate
+                "a1", #SKN-1 activation of med-2
+                "a2", #SKN-1 activation of med-1
+                "a3", #SKN-1 activation of end-3
+                "a4", #SKN-1 activation of end-1
+                "a6", #MED-2 activation of end-3
+                "a17", #MED-2 activation of end-1
+                "a7", #MED-1 activation of end-3
+                "a8", #MED-1 activation of end-1
+                "a9", #POP-1 activation of end-3
+                "a10", #POP-1 activation of end-1
+                "a12", #END-3 activation of elt-7
+                "a13", #END-1 activation of elt-7
+                "a14", #END-3 activation of elt-2
+                "a15", #END-1 activation of elt-2
+                "f1", #ELT-7 feedback
+                "f2", #ELT-2 feedback
+                "theta1", #Threshold required for ELT-7 feedback (ppm)
+                "theta2", #Threshold required for ELT-2 feedback (ppm)
+                "tau") #Transcription-activation delay (min)
+
+# Parameter values used in the paper and listed in the supplementary file
+bestParams <- c(
+  3.278295e-02, #delta, Native degradation rate
+  4.666656e-03, #a1, SKN-1 activation of med-2
+  3.789606e-02, #a2, SKN-1 activation of med-1
+  2.546014e-02, #a3, SKN-1 activation of end-3
+  7.088873e-18, #a4, SKN-1 activation of end-1
+  3.473743e-02, #a6, MED-2 activation of end-3
+  4.436445e-18, #a17, MED-2 activation of end-1
+  2.181668e-17, #a7, MED-1 activation of end-3
+  2.048436e-01, #a8, MED-1 activation of end-1
+  7.410022e-02, #a9, POP-1 activation of end-3
+  8.742976e-17, #a10, POP-1 activation of end-1
+  1.074840e-01, #a12, END-3 activation of elt-7
+  1.559075e-17, #a13, END-1 activation of elt-7
+  2.219584e-18, #a14, END-3 activation of elt-2
+  3.344716e-02, #a15, END-1 activation of elt-2
+  1.185834e-01, #f1, ELT-7 feedback
+  3.839391e-01, #f2, ELT-2 feedback
+  1.216969e+02, #theta1, Threshold required for ELT-7 feedback (ppm)
+  2.141869e+01, #theta2, Threshold required for ELT-2 feedback (ppm)
+  5.184828e+01 #tau, Transcription-activation delay (min)
+)
+
+#If iterating on previously fitted values, parameter guesses will be drawn from bestParamGuesses data frame in the workspace.
+#If running for the first time, bestParamGuesses will be reset to the published values.
+if(!iterating){
+  bestParamGuesses <- data.frame(parameter = paramNames, guess = bestParams)
+}
+
+# This line draws the parameter values back out of the bestParamGuesses data frame (to allow for iteration)
+bestParams <- bestParamGuesses[, "guess"]
+nParams <- length(bestParams)
+
+#Switches to make knockout mutants
+#Currently not used -- leave as FALSE
+skn1ko <- FALSE
+pop1ko <- FALSE
+med2ko <- FALSE
+med1ko <- FALSE
+end3ko <- FALSE
+end1ko <- FALSE
+elt7ko <- FALSE
+elt2ko <- FALSE
+
+# Time settings
+tEnd <- 200 #min
+dt <- 0.01 #time step for Euler approximation
+nTimepoints <- tEnd/dt + 1
+times <- seq(0, tEnd, by = dt)
+
+#Initial conditions (ppm)
+skn1_0 <- 23
+pop1_0 <- 3
+med2_0 <- 13.8
+med1_0 <- 5.6
+end3_0 <- 5.3
+end1_0 <- 0
+elt7_0 <- 0
+elt2_0 <- 0
+
+#SKN-1 and POP-1 settings for square wave
+skn1offTime <- 23 #min
+pop1onTime <- 23 #min
+pop1offTime <- 41 #min
+
+####################################################################################################
+
+# Load transcriptomics data used to fit model (units of time and ppm)
+time <- c(0,23,41,53,66,83,101,122,143,186)
+med2 <- c(13.8,13.8,0,0,0,0,0,0,0,0)
+med1 <- c(5.6,5.3,0.4,0,0,0,0,0,0,0)
+end3 <- c(5.3,11.0,18.0,17.0,17.0,23.0,0,0,0,0)
+end1 <- c(0,7,17,26,35,49,62,36,45,0)
+elt7 <- c(0,0.5,9,12,27,24,38,47,36,27)
+elt2 <- c(0,0,0,0,0,0,0,0,53,134)
+FFdataFit <- data.frame(time,med2,med1,end3,end1,elt7,elt2)
+
+####################################################################################################
+
+#Begin evolution (fitting) of model with nGen generations (cycles) of iteration
+for(gen in 1:nGen){
+  
+  #Print progress to the console every 100 generations
+  if(gen/100 == round(gen/100)){
+    cat("Simulation ", gen, " of ", nGen, ", sum of squares = ", leastSquares, "\n")
+  }
+  
+  #Transfer parameter values to another vector for mutation and testing
+  testParams <- bestParams
+  
+  # Introduce mutations (change random parameter values randomly)
+  mutation <- runif(nParams) < mu #Creates logical vector of length nParams dictating whether each parameter will be changed (TRUE) or not (FALSE)
+  newParams <- abs(rnorm(nParams, mean = 1, sd = mutationSD)*testParams) # new values to be assigned to parameters for which mutation is TRUE
+  testParams <- testParams*(!mutation) + newParams*mutation #combine current values for unchanged parameters with new values for mutated parameters
+  
+  # Store parameter values to be tested in their respective named vectors
+  delta <- testParams[1]
+  a1 <- testParams[2]
+  a2 <- testParams[3]
+  a3 <- testParams[4]
+  a4 <- testParams[5]
+#  a5 <- testParams[6]
+  a6 <- testParams[6]
+  a17 <- testParams[7]
+  a7 <- testParams[8]
+  a8 <- testParams[9]
+  a9 <- testParams[10]
+  a10 <- testParams[11]
+#  a11 <- testParams[12]
+  a12 <- testParams[12]
+  a13 <- testParams[13]
+  a14 <- testParams[14]
+  a15 <- testParams[15]
+#  a16 <- testParams[14]
+  f1 <- testParams[16]
+  f2 <- testParams[17]
+  theta1 <- testParams[18]
+  theta2 <- testParams[19]
+  tau <- testParams[20]
+  
+  #Calculate number of timepoints represented by transcription-activation delay tau
+  txnDelayTimepoints <- round(tau/dt)
+  
+  #SKN-1 and POP-1 initial on or off settings
+  skn1on <- TRUE
+  pop1on <- FALSE
+
+  #Creating vectors to store expression level time series for all genes
+  med2 <- numeric(nTimepoints)
+  med1 <- numeric(nTimepoints)
+  end3 <- numeric(nTimepoints)
+  end1 <- numeric(nTimepoints)
+  elt7 <- numeric(nTimepoints)
+  elt2 <- numeric(nTimepoints)
+  med2[1] <- med2_0*(-1*(med2ko - 1))
+  med1[1] <- med1_0*(-1*(med1ko - 1))
+  end3[1] <- end3_0*(-1*(end3ko - 1))
+  end1[1] <- end1_0*(-1*(end1ko - 1))
+  elt7[1] <- elt7_0*(-1*(elt7ko - 1))
+  elt2[1] <- elt2_0*(-1*(elt2ko - 1))
+  
+  # This is the model code, mostly the same as in endoderm_GRN_model.R
+  for(i in 1:(nTimepoints-1)){
+    
+    # Logical gates to create square waves for SKN-1 and POP-1
+    if(times[i] > skn1offTime){
+      skn1on <- FALSE
+    }
+    if(times[i] > pop1onTime){
+      pop1on <- TRUE
+    }
+    if(times[i] > pop1offTime){
+      pop1on <- FALSE
+    }
+    
+    #Conditions at t_i
+    skn1 <- skn1_0*skn1on*!skn1ko
+    pop1 <- pop1_0*pop1on*!pop1ko
+    med2_i <- med2[i]
+    med1_i <- med1[i]
+    end3_i <- end3[i]
+    end1_i <- end1[i]
+    elt7_i <- elt7[i]
+    elt2_i <- elt2[i]
+    
+    # Calculation of timepoint used for activation due to transcription-activation delay
+    txnEffectTimepoint <- max(c(1, i - txnDelayTimepoints))
+    med2_prev <- med2[txnEffectTimepoint]
+    med1_prev <- med1[txnEffectTimepoint]
+    end3_prev <- end3[txnEffectTimepoint]
+    end1_prev <- end1[txnEffectTimepoint]
+    elt7_prev <- elt7[txnEffectTimepoint]
+    elt2_prev <- elt2[txnEffectTimepoint]
+    
+    # System of differential equations described in supplemental text
+    # Conditions at t_i used to calculate rates of change
+    dmed2 <- (skn1*a1 - delta*med2_i)*!med2ko
+    dmed1 <- (skn1*a2 - delta*med1_i)*!med1ko
+    dend3 <- (skn1*a3 + med1_prev*a7 + med2_prev*a6 + pop1*a9 - delta*end3_i)*!end3ko
+    dend1 <- (skn1*a4 + med2_prev*a17 + med1_prev*a8 + pop1*a10 - delta*end1_i)*!end1ko
+    delt7 <- (end1_prev*a13 + end3_prev*a12 + f1*(elt7_prev>theta1)*elt7_prev - delta*elt7_i)*!elt7ko
+    delt2 <- (end1_prev*a15 + end3_prev*a14 + f2*(elt2_prev>theta2)*elt2_prev - delta*elt2_i)*!elt2ko
+    
+    # Calculation of conditions at t_i+1 given the derivatives and time step dt
+    med2[i+1] <- med2_i + dt*dmed2
+    med1[i+1] <- med1_i + dt*dmed1
+    end3[i+1] <- end3_i + dt*dend3
+    end1[i+1] <- end1_i + dt*dend1
+    elt7[i+1] <- elt7_i + dt*delt7
+    elt2[i+1] <- elt2_i + dt*delt2
+  }
+  
+  # Calculate sum of squared differences between model predictions and transcriptomics data
+  simData <- data.frame(med2, med1, end3, end1, elt7, elt2)
+  sumOfSquares <- 0
+  for(i in 1:nrow(FFdataFit)){
+    fitTime <- FFdataFit[i,"time"]
+    sumOfSquares <- sumOfSquares + sum((FFdataFit[i, 2:7] - simData[which(times == fitTime),])^2)
+  }
+  
+  # If sum of squares is less than before, replace parameter values with testParams and plot the model run
+  # Otherwise, bestParams remains intact
+  if(sumOfSquares < leastSquares){
+    leastSquares <- sumOfSquares
+    bestParams <- testParams
+    plot(y=med2, x=times, type = "l", ylab = "[Transcript] (ppm)", xlab = "Time post 4-cell stage (min)", 
+         col = "red", ylim = c(0,200), xlim = c(0,200),
+         main = graphTitle)
+    lines(y=med1, x=times, type = "l", col = "orange")
+    lines(y=end3, x=times, type = "l", col = "green")
+    lines(y=end1, x=times, type = "l", col = "skyblue")
+    lines(y=elt7, x=times, type = "l", col = "blue")
+    lines(y=elt2, x=times, type = "l", col = "purple")
+    legend("topleft", legend = c("med-2", "med-1", "end-3", "end-1", "elt-7", "elt-2"),
+           col = c("red", "orange", "green", "skyblue", "blue", "purple"), lwd = 1)
+  }
+}
+
+#Make graph of gene expression levels for final parameter values
+plot(y=med2, x=times, type = "l",
+     ylab = "[Transcript] (ppm)", xlab = "Time post 4-cell stage (min)", 
+     col = "red", ylim = c(0,200), xlim = c(0,200),
+     main = graphTitle)
+lines(y=med1, x=times, type = "l", col = "orange")
+lines(y=end3, x=times, type = "l", col = "green")
+lines(y=end1, x=times, type = "l", col = "skyblue")
+lines(y=elt7, x=times, type = "l", col = "blue")
+lines(y=elt2, x=times, type = "l", col = "purple")
+legend("topleft", legend = c("med-2", "med-1", "end-3", "end-1", "elt-7", "elt-2"),
+       col = c("red", "orange", "green", "skyblue", "blue", "purple"), lwd = 1)
+
+# Store evolved parameters and print to console
+bestParamGuesses <- data.frame(parameter = paramNames, guess = bestParams)
+print(bestParamGuesses)
